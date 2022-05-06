02c1448f8acb9b8cf3f43b9df41bff7ae798106a
Adam G <adagun@hotmail.com>
2022-05-06 22:03:09 +0200

    fix multiple codes getting added

--- qr.js
+++ qr.js
@@ -1,12 +1,14 @@
 function generateQR() {
+    
   var userInput = document.getElementById("inputText").value;
-  var element = document.getElementById("generatedCode");
-  let qrcode = new QRCode(element, {
+  var codeOutput = document.getElementById("generatedCode");
+  codeOutput.innerHTML = ""; // stop from adding multiple codes
+  let qrcode = new QRCode(codeOutput, {
     text: userInput ? userInput : "test",
     width: 250,
     height: 250,
     colorDark: "#000000",
     colorLight: "#FFFFFF",
-     correctLevel : QRCode.CorrectLevel.H
+    correctLevel: QRCode.CorrectLevel.H,
   });
 }
