50e07d1f254ba0132fd936c96ccf72bd32b856ba
Adam G <adagun@hotmail.com>
2022-05-06 22:27:44 +0200

    added toastify and copy function

--- qr.js
+++ qr.js
@@ -1,5 +1,4 @@
 function generateQR() {
-    
   var userInput = document.getElementById("inputText").value;
   var codeOutput = document.getElementById("generatedCode");
   codeOutput.innerHTML = ""; // stop from adding multiple codes
@@ -11,4 +10,25 @@ function generateQR() {
     colorLight: "#FFFFFF",
     correctLevel: QRCode.CorrectLevel.H,
   });
+}
+
+function copy() {
+  var img = document.querySelector("#generatedCode").querySelector("img");
+  const canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
+  canvas.toBlob((blob) => {
+    navigator.clipboard.write([
+      new ClipboardItem({
+        "image/png": blob,
+      }),
+    ]);
+  }, "image/png");
+  var myToast = Toastify({
+    text: "Image copied to clipboard",
+    duration: 5000,
+ 
+   })
+   myToast.showToast();
 }
